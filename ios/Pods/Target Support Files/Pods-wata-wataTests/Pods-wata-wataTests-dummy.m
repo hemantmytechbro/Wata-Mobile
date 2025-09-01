8640856132b0cd7514e342a9782559c4b1b4b28b
hemantmytechbro <hemant.s@mytechbro.com>
2025-09-01 12:50:03 +0530

    fixed issues

---
+++ ios/Pods/Target Support Files/Pods-wata-wataTests/Pods-wata-wataTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wata_wataTests : NSObject
+@end
+@implementation PodsDummy_Pods_wata_wataTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-wata/Pods-wata-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wata : NSObject
+@end
+@implementation PodsDummy_Pods_wata
+@end
