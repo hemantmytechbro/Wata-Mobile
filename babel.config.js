4f062757d4f000ec50ececca1e3cc3ff4cc3604a
hemantmytechbro <hemant.s@mytechbro.com>
2025-09-01 15:51:41 +0530

    Fixes issue

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  presets: ['@react-native/babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@': './src',
-        },
-      },
-    ],
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
