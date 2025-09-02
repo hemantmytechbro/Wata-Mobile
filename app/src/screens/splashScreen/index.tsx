5d3f298b3aefe042b737e1188fe67578c8446b2e
hemantmytechbro <hemant.s@mytechbro.com>
2025-09-02 15:31:45 +0530

    fixed issue

---
+++ app/src/screens/splashScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {View,Text} from "react-native";
+
+const splashScreen = () =>{
+    
+    return(
+        <View>
+            <Text>Splash Screen</Text>
+        </View>
+    );
+}
+
+export default splashScreen;
